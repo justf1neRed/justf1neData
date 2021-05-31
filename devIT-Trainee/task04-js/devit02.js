577e4eb911f3d91970ac7a84bea6cbf83dee7ffa
justf1neRed <olegjustfine@gmail.com>
2021-05-31 10:56:32 +0300

    chan
    chan

--- devIT-Trainee/task04-js/devit02.js
+++
@@ -1,9 +0,0 @@
-// 2. Свертка. Используйте метод reduce в комбинации с concat для свёртки массива массивов в один массив, у которого есть все элементы входных массивов.
-
-var arrays = [[1, 2, 3], [4, 5], [6]];
-
-arrays = arrays.reduce(function(x, y){
-    return x.concat(y);
-});
-
-console.log(arrays);
